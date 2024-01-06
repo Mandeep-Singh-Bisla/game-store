7726efbbf2142d66ee3bba6abed10b76a6e178ff
Mandeep Singh <navdeep@MacBook-Air.local>
2024-01-06 14:09:15 +0530

    A genric hook for games and genreList

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 const skeletons = [1, 2, 3, 4, 5, 6];
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -16,7 +16,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,33 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-import { CanceledError } from "axios";
-interface Genres{
+import useData from "./useData";
+export interface Genres{
     id:number,
     name:string
 }
-interface FetchGenresResponse{
-    count:number,
-    results:Genres[]
-}
-const useGenre=()=>{
-    const [genres, setGenres] = useState<Genres[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading,setLoading]=useState(false);
-    useEffect(() => {
-        const controller=new AbortController();
-        setLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres",{signal:controller.signal})
-      .then((res) => {setGenres(res.data.results)
-        setLoading(false);})
-      .catch((err) => {
-        if(err instanceof CanceledError)
-        return ;
-        setError(err.message)
-        setLoading(false);
-    }); 
-      return ()=>controller.abort();
-  },[]);
-  return {genres,error,isLoading};
-}
+const useGenre=()=>useData<Genres>('/genres')
 export default useGenre;
