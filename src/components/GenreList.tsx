d4f9c1f84d5779c28188d6496a6367b795b13aee
Mandeep Singh <navdeep@MacBook-Air.local>
2024-01-18 13:26:24 +0530

    Highlight the selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,10 +10,11 @@ import {
 } from "@chakra-ui/react";
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImage from "../services/image-url";
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
 
@@ -29,6 +30,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               borderRadius={8}
             ></Image>
             <Button
+              fontWeight={genre.id == selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               variant="link"
               fontSize="lg"
